f15c75228d18240a2ab57fa6047f8f6e92c5081a
jonny201251 <jonny201251@163.com>
2020-03-27 15:03:19 +0800

    app

--- utils/ItemUtil.js
+++
@@ -1,8 +0,0 @@
-export default class ItemUtil {
-    static get(){
-        //商品列表数据
-        fetch(hostPath+'/spu?currentPage')
-            .then(res => res.json())
-            .then(resp => this.setState({images: resp.data}))
-    }
-}
