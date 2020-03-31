7454bdb6d3e7c2884c34149565fef7b89eb1b149
jonny201251 <jonny201251@163.com>
2020-03-31 15:30:11 +0800

    购物车页

--- pages/tabbars/Me.js
+++ pages/tabbars/Me.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react'
-import {AppRegistry, StyleSheet, View, Text, Image, TouchableHighlight} from 'react-native'
+import {AppRegistry, StyleSheet, View, Text, Image, TouchableHighlight, AsyncStorage} from 'react-native'
+import Constants from "../../utils/constants"
+import StorageUtil from '../../utils/StorageUtil'
 
+const hostPath = Constants.hostPath
 export default class Me extends Component {
+    componentWillMount() {
+        //用户登录
+        StorageUtil.get("userInfo").then(user => {
+            if (user == null) {
+                fetch(hostPath + '/app/login?username=范海峰&password=1')
+                    .then(res => res.json())
+                    .then(resp => {
+                        StorageUtil.set("userInfo", resp.data)
+                    })
+            }
+        })
+    }
+
     render() {
         return <View style={{flex: 1, alignItems: 'center', backgroundColor: 'white'}}>
             <Text style={{margin: 50}}>我的</Text>
--- pages/tabbars/ShopCart.js
+++ pages/tabbars/ShopCart.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react'
-import {AppRegistry, StyleSheet, View, Text, Image, TouchableHighlight} from 'react-native'
+import {AppRegistry, StyleSheet, View, Text, Image, TouchableHighlight, ScrollView, AsyncStorage} from 'react-native'
+import {Tabs, Carousel, Flex, Button, WhiteSpace, WingBlank, Card} from '@ant-design/react-native'
+import Constants from '../../utils/constants'
+import StorageUtil from '../../utils/StorageUtil'
+
+const hostPath = Constants.hostPath
 
 export default class ShopCart extends Component {
+    state = {}
+
+    componentWillMount() {
+        StorageUtil.get("userInfo").then(user => {
+            if (user) {
+                console.warn(user)
+            }
+        })
+
+
+    }
+
     render() {
         return <View style={{flex: 1, alignItems: 'center', backgroundColor: 'white'}}>
-            <Text style={{margin: 50}}>购物车</Text>
+            <Text style={{margin: 50}}>购物车-------------------------------</Text>
         </View>
     }
 }
