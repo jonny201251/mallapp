a337dbd13e7e88cf358baf4f654dea4019fabdc3
jonny201251 <jonny201251@163.com>
2020-03-31 19:23:26 +0800

    购物车页

--- pages/tabbars/ShopCart.js
+++ pages/tabbars/ShopCart.js
@@ -7,11 +7,26 @@ import StorageUtil from '../../utils/StorageUtil'
 const hostPath = Constants.hostPath
 
 export default class ShopCart extends Component {
-    state = {}
+    state = {
+        totalMoney: 0.00
+    }
 
     componentWillMount() {
         StorageUtil.get("userInfo").then(user => {
             if (user) {
+                //收货地址
+                StorageUtil.get("receiveAddress").then(value => {
+                    if (value == null) {
+                        fetch(hostPath + '/app/receiveAddress?userId=' + user.id)
+                            .then(res => res.json())
+                            .then(resp => {
+                                StorageUtil.set("receiveAddress", resp.data)
+                                this.setState({receiveAddress: resp.data})
+                            })
+                    } else {
+                        this.setState({receiveAddress: value})
+                    }
+                })
                 //获取购物车中的商品
                 fetch(hostPath + '/app/cart/list?userId=' + user.id)
                     .then(res => res.json())
@@ -25,8 +40,25 @@ export default class ShopCart extends Component {
     }
 
     render() {
-        return <View style={{flex: 1, alignItems: 'center', backgroundColor: 'white'}}>
-            <Text style={{margin: 50}}>购物车-------------------------------</Text>
+        return <View style={{flex: 1, backgroundColor: '#F8F8F8'}}>
+            <Text style={{marginTop: 50, textAlign: 'center'}}>购物车</Text>
+            <Text style={{color: 'grey', margin: 10}}>
+                {this.state.receiveAddress ? this.state.receiveAddress.address : ''}
+            </Text>
+            <ScrollView>
+                <Image source={{uri: 'http://192.168.99.233:8080/mall/image/item/15271057fbe64701bb39237dc17a832b.jpg'}}
+                       style={{width: '99.8%', height: 900}}/>
+            </ScrollView>
+            <View style={{flexDirection: 'row'}}>
+                <Text style={{
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: '#c81623',
+                    height: 50,
+                    lineHeight: 50
+                }}>总计:¥{this.state.totalMoney}</Text>
+                <Button type="warning" style={{width: '30%'}}>去结算</Button>
+            </View>
         </View>
     }
 }
