1520628bb4588b5aeff44a5851f42a00a17c0b55
jonny201251 <jonny201251@163.com>
2020-04-03 09:29:45 +0800

    部署

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 export default {
-    //部署时的真实IP地址和项目名称
-    // hostPath: "http://haiyingmall.paas.casicloud.com"
+    //开发时的IP地址和项目名称
     hostPath: "http://192.168.99.233:8080/mall"
+    //部署时放开
+    // hostPath: "http://haiyingmall.paas.casicloud.com"
 }
